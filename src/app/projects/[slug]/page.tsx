8f2c1e28711c18789f37fbc0f5621e08d194eaa5
teto-ri <tensi3165@naver.com>
2024-03-18 23:44:55 +0900

    update category

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -28,7 +28,7 @@ export default async function ProjectPage({
         className="mx-auto mt-40 text-center"
       >
         <h2 className="mb-4 text-3xl font-bold">Post Not Found</h2>
-        <Link href="/project">
+        <Link href="/projects">
           <span className="mr-2">&larr;</span>
           <span>Go to list page</span>
         </Link>
