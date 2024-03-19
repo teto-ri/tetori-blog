4ae4f3e6e251eb145881e37994a177efe3fc5070
teto-ri <tensi3165@naver.com>
2024-03-19 21:16:19 +0900

    set default theme dark

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -2,11 +2,18 @@
 
 import { useTheme } from 'next-themes';
 import { BsMoon, BsMoonFill, BsSun } from 'react-icons/bs';
+import { useEffect } from 'react';
 
 import useMounted from '@/hooks/use-mounted';
 
 export default function ThemeToggle() {
   const { theme, setTheme } = useTheme();
+  useEffect(() => {
+    if (!theme) {
+      setTheme('light');
+    }
+  }, [theme, setTheme]);
+
   const mounted = useMounted();
   const iconCN = 'z-10 h-8 w-8 p-[0.4rem]';
 
