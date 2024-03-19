7eca42def647d8aa3342ef6344ed0a54adc11c12
teto-ri <tensi3165@naver.com>
2024-03-19 21:17:57 +0900

    set default theme dark

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -10,7 +10,7 @@ export default function ThemeToggle() {
   const { theme, setTheme } = useTheme();
   useEffect(() => {
     if (!theme) {
-      setTheme('light');
+      setTheme('dark');
     }
   }, [theme, setTheme]);
 
