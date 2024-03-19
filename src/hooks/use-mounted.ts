62cec7c0bb19e1a4a61e14f3675f6652e8e1a274
teto-ri <tensi3165@naver.com>
2024-03-19 22:23:36 +0900

    Revert "fix localstorage"
    This reverts commit a834970ecd15918f0a21c8c56ccdb00c8db57663.

--- src/hooks/use-mounted.ts
+++ src/hooks/use-mounted.ts
@@ -2,29 +2,14 @@ import { useEffect, useState } from 'react';
 import { useTheme } from 'next-themes';
 
 export default function useMounted() {
-  const { theme, setTheme } = useTheme();
+  const { setTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
-    const storedTheme = localStorage.getItem('theme');
-
-    if (storedTheme) {
-      setTheme(storedTheme);
-    } else {
-      const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
-      const defaultTheme = isDarkMode ? 'dark' : 'light';
-      setTheme(defaultTheme);
-      localStorage.setItem('theme', defaultTheme);
-    }
-
+    const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    setTheme(isDarkMode ? 'dark' : 'light');
     setMounted(true);
   }, [setTheme]);
-
-  useEffect(() => {
-    if (theme) {
-      localStorage.setItem('theme', theme);
-    }
-  }, [theme]);
 
   return mounted;
 }
